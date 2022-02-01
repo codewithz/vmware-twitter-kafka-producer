cd8ed6825cc192e08db42bd3a0d517412ff3b210
Zartab Nakhwa <zartab@codewithz.com>
2022-02-01 15:27:06 +0530

    Twitter Connected to Java

---
+++ src/main/java/com/vmware/kafka/TwitterProducer.java
@@ -0,0 +1,110 @@
+package com.vmware.kafka;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterProducer {
+
+    Logger logger= LoggerFactory.getLogger(TwitterProducer.class);
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+
+    public void run(){
+
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+        //Create a Twitter Client
+        Client client=createTwitterClient(msgQueue);
+        //Attempt to establish a connection
+        client.connect();
+
+        //Create a Kafka Producer
+        KafkaProducer<String,String> producer=createKafkaProducer();
+        // Loop to send the tweets to Kafka
+        // on a different thread, or multiple different threads....
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(msg!=null){
+                logger.info(msg);
+                ProducerRecord<String,String> record=
+                        new ProducerRecord<>("vmware_twitter_tweets",null,msg);
+                producer.send(record, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        if(e!=null){
+                            logger.error("Something bad happened",e);
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    public Client createTwitterClient(BlockingQueue<String> msgQueue){
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        // Optional: set up some followings and track terms
+
+        List<String> terms = Lists.newArrayList("bitcoin");
+
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        Authentication hosebirdAuth =
+                new OAuth1("LWw5Kj7I9ZPKWNgBpEixaWoCi",
+                        "JpVe7PwYhmPrzBRDGqxUgjx4fY3329ErSGfsD6s9EYaQ1DyeSZ",
+                        "3244586185-OQshPQIq9gKRuaoRGllx9dEggQRIgQyJBNFUZub",
+                        "XbxlSuVivihQA34eIsEsLRwDFQKuHipu8AzhxLnlMlQyg");
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        Client hosebirdClient= builder.build();
+        return hosebirdClient;
+
+
+    }
+
+    public KafkaProducer<String,String> createKafkaProducer(){
+        String bootstrapServers="127.0.0.1:9092";
+        //Create Producer Properties
+        Properties properties=new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //Create the Producer
+        KafkaProducer<String,String> producer=new KafkaProducer<String, String>(properties);
+        return producer;
+    }
+
+}
